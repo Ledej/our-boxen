55f65a0200d2caa3035e549480c0a91378e24c0c
Pierre Dulac <dulacpier@gmail.com>
2013-12-16 08:58:22 +0100

    Add python 2.7.4 to mimick heroku, numpy, geos and gdal

--- modules/projects/manifests/ledej.pp
+++ modules/projects/manifests/ledej.pp
@@ -2,13 +2,38 @@ class projects::ledej {
   include redis
   include postgresql
   include elasticsearch
+  include python
+  include python::virtualenvwrapper
+
+  python::mkvirtualenv{ 'venv':
+    ensure      => present,
+    systempkgs  => false,
+    distribute  => true,
+    project_dir => "~/src/ledej",
+  }
+
+  python::requirements { 'reqs-dev':
+    requirements => '/Users/peteralaoui/src/ledej/reqs/dev.txt',
+    virtualenv   => 'venv',
+  }
+
+  python::pip { 'numpy':
+    ensure     => present,
+    virtualenv => $python::config::global_venv
+  }
+
+  package { 
+    'geos':
+      ensure => present;
+    'gdal':
+      ensure => present;
+  }
 
   boxen::project { 'ledej':
-    dotenv        => true,
+    # dotenv        => true,
     elasticsearch => true,
     postgresql    => true,
     redis         => true,
-    # python        => '2.7.5',
-    source        => 'Ledej/ledej-website'
+    source        => 'Ledej/ledej-website',
   }
 }
