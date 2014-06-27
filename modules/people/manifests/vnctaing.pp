8ae5c78458ba697cb1ca2cfa72c972ebc6a505c1
Pierre Dulac <dulacpier@gmail.com>
2014-06-27 12:24:51 +0200

    Add vincent personal file

---
+++ modules/people/manifests/vnctaing.pp
@@ -0,0 +1,48 @@
+class people::vnctaing {
+  include teams::mainstream
+  
+  include alfred
+  include git
+
+
+  $home = "/Users/${::boxen_user}"
+  $srcdir = "${boxen::config::srcdir}"
+
+
+  ## 
+  # Dotfiles
+
+  $dotfiles = "${srcdir}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'vnctaing/dotfiles',
+    require => [
+      File[$srcdir],
+      Exec["install-default-dotfiles"],
+    ],
+    notify  => Exec['install-dotfiles'],
+  }
+
+  exec { "install-dotfiles":
+    cwd         => $dotfiles,
+    command     => "make",
+    refreshonly => true
+  }
+
+  # override the git global config
+  Git::Config::Global <| title == 'core.excludesfile' |> {
+    value   => "~/.gitignore_global",
+    require => Exec["install-dotfiles"]
+  }
+
+
+  ##
+  # OSX preferences
+  
+  Boxen::Osx_defaults {
+    user => $::boxen_user,
+  }
+
+  include osx::dock::autohide
+  include osx::global::natural_mouse_scrolling
+}
